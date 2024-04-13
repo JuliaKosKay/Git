76a326c26ba80e9bc34093262df19915699a304c
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-13 23:37:24 +0300

    учебное

---
+++ mod/smth.py
@@ -0,0 +1,7 @@
+def smth(a, b):
+    res = a + b
+    print('sum equal: ' + str(res))
+
+def sub(a, b):
+    result = a - b
+    print('sub equal: ' + str(result))
--- start.py
+++ start.py
@@ -1,10 +1,22 @@
 #import edu2
-import math
+import mod
 import random
 r = random.randrange(0, 10)
-print(math.pi)
+print(mod.pi)
 edu2.some()
 print(r)
 user = 'User'
 user_random = user + str(r) #User6
 print(user_random)
+
+import math
+print(math.pi) #3.141592653589793
+from mod import math
+print(math.pi)#5
+
+from mod import smth
+smth.sub(10, 15)
+smth.smth(18, 5)
+
+
+

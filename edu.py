6eb06c889939adc77339cb642a471f16a1ef3699
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-13 16:51:11 +0300

    учебное

--- edu.py
+++ edu.py
@@ -10,5 +10,37 @@ def hi(name, age):
 
 hi(name, '32')
 
+age = 32
+
+if age == 32:
+    print('I am 32')
+elif age>28:
+    print('My age more than 32')
+else:
+    print('My age less than 32')
+
+if 'l' in name:
+    print('I am Julia')
+else:
+    print('It is not my name')
+
+pin = 1234
+print('input your pin, please')
+user_pin = int(input())
+
+if pin == user_pin:
+    print('What amount do you want to withdraw?')
+else:
+    print('Wrong pin, 2 tries left')
+    user_pin = int(input())
+    if pin == user_pin:
+        print('What amount do you want to withdraw?')
+    else:
+        user_pin = int(input())
+        print('Wrong pin, 1 try left')
+        if pin == user_pin:
+            print('What amount do you want to withdraw?')
+        else:
+            print('Card have been blocked, please, call your bank')
 
 

cf63025cda0f3b629a7e3c0f24730bff5e409440
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-14 00:10:27 +0300

    учебное

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-class Person():
+'''class Person():
 
     def __init__(self, name, age):
         self.name = name
@@ -19,6 +19,27 @@ man.dance()
 woman = Person('Alice', 22)
 print(woman.name)
 print(woman.age)
-woman.sing()
+woman.sing()'''
+
+class People():
+   def __init__(self, name, age, height, weight):
+       self.name = name
+       self.age = age
+       self.height = height
+       self.weight = weight
+
+   def descr(self):
+        descr = self.name + ' ,he is ' + str(self.age) + ' and his height is ' + str(self.height) + ' ,also his weight is ' + str(self.weight)
+        print('his name is ' + descr)
+   def upd_weight(self, kg):
+       self.weight = kg
+
+guy = People('Liam', 30, 190, 90)
+guy.upd_weight(78)
+guy.descr()
+
+
+
+
 
 

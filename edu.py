a5945594400a3093401c9ff263ec875efa3afbb8
JuliaKosKay <166844087+JuliaKosKay@users.noreply.github.com>
2024-04-13 20:16:18 +0300

    учебное

--- edu.py
+++ edu.py
@@ -2,13 +2,17 @@ def summ(num1, num2):
     res = num1 + num2
     print(res)
 
+
 summ(50, 30)
 
 name = 'Julia'
+
+
 def hi(name, age):
     print('Hi I am ' + name + ' and I am ' + age)
 
 hi(name, '32')
+
 
 age = 32
 
